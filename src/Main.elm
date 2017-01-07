2d150fe1f6b4a8c9bfd98bc8f8d01b8f22c00153
Denys Batrak <baden.i.ua@gmail.com>
2017-01-07 17:27:06 +0200

    Грубый набросок для добавления/удаления портов.

--- src/Main.elm
+++ src/Main.elm
@@ -4,44 +4,150 @@
 
 module Main exposing (..)
 
-import Html exposing (beginnerProgram, div, button, text)
+import Html exposing (Html, div, button, text, select, option)
+import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 
 
+type alias Port =
+    { id : Int }
+
+
 type alias Model =
-    { counter : Int
+    { uid : Int
+    , ports : List Port
     }
 
 
-init : Model
+initModel : Model
+initModel =
+    { uid = 0
+    , ports = []
+    }
+
+
+type Msg
+    = AddPort
+    | AddLabel
+    | RemovePort Int
+
+
+init : ( Model, Cmd Msg )
 init =
-    { counter = 0 }
+    ( initModel, Cmd.none )
 
 
 main : Program Never Model Msg
 main =
-    beginnerProgram { model = init, view = view, update = update }
+    Html.program { init = init, view = view, update = update, subscriptions = subscriptions }
 
 
-view : Model -> Html.Html Msg
-view model =
-    div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (toString model.counter) ]
-        , button [ onClick Increment ] [ text "+" ]
+initPort : Int -> Port
+initPort id =
+    { id = id
+    }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        AddPort ->
+            let
+                port_ =
+                    initPort model.uid
+            in
+                { model
+                    | uid = model.uid + 1
+                    , ports = model.ports ++ [ port_ ]
+                }
+                    ! []
+
+        AddLabel ->
+            model ! []
+
+        RemovePort id ->
+            { model | ports = List.filter (\t -> t.id /= id) model.ports }
+                ! []
+
+
+control_view : Model -> Html Msg
+control_view model =
+    div [ class "control" ]
+        [ button [ onClick AddPort ] [ text "Добавить порт" ]
+        , button [ onClick AddLabel ] [ text "Поставить метку" ]
         ]
 
 
-type Msg
-    = Increment
-    | Decrement
+toSelectOption : String -> Html a
+toSelectOption x =
+    option [] [ text x ]
 
 
-update : Msg -> Model -> Model
-update msg model =
-    case msg of
-        Increment ->
-            { model | counter = model.counter + 1 }
+listToHtmlSelectOptions : List String -> List (Html a)
+listToHtmlSelectOptions list =
+    list
+        |> List.map toSelectOption
 
-        Decrement ->
-            { model | counter = model.counter - 1 }
+
+fakePortList : List String
+fakePortList =
+    [ "Выберите порт:"
+    , "COM1"
+    , "COM2"
+    , "COM3"
+    ]
+
+
+fakeSpeedList : List String
+fakeSpeedList =
+    [ "Выберите скорость:"
+    , "1200"
+    , "2400"
+    , "4800"
+    , "9600"
+    , "19200"
+    , "38400"
+    , "57600"
+    , "115200"
+    , "230400"
+    , "460800"
+    , "125000"
+    , "166667"
+    , "250000"
+    , "500000"
+    ]
+
+
+port_view : Port -> Html Msg
+port_view port_ =
+    div [ class "port" ]
+        [ select [] (listToHtmlSelectOptions fakePortList)
+        , select [] (listToHtmlSelectOptions fakeSpeedList)
+        , button
+            []
+            [ text "Подключить" ]
+        , button [ onClick (RemovePort port_.id) ] [ text "Удалить" ]
+        , text (toString port_)
+        ]
+
+
+ports_view : List Port -> Html Msg
+ports_view ports =
+    ports
+        |> List.map (\c -> port_view c)
+        |> div [ class "port_list" ]
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ ports_view model.ports
+        , control_view model
+        , div [] [ text (toString model) ]
+        ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    -- Time.every second Tick
+    Sub.none
