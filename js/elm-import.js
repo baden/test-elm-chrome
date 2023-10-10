61ea739654457423da983182ae29e030f2506f99
Денис <baden.i.ua@gmail.com>
2023-10-10 18:01:48 +0300

    Updates

---
+++ js/elm-import.js
@@ -0,0 +1,102 @@
+/* jslint laxcomma: true, esversion: 6 */
+
+// var gui = require('nw.gui');
+// var win = gui.Window.get();
+//
+// function showDevTools() {
+//   win.showDevTools();
+// }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+let ports = {};
+
+var app = Elm.Main.init({
+    node: document.getElementById('widget'),
+    flags: Date.now()
+});
+
+app.ports.getPort.subscribe(async function(id) {
+    // socket.send(message);
+    console.log('getPorts', id);
+
+    const port = await navigator.serial.requestPort();
+    ports[id] = port;
+
+    console.log('port', port, port.getInfo());
+    const vendorId = port.getInfo().usbVendorId.toString(16);
+    const productId = port.getInfo().usbProductId.toString(16);
+    console.log('vendorId', vendorId);
+    console.log('productId', productId);
+
+    app.ports.onPortGeted.send(id);
+
+    // await port.open({ baudRate: 9600 });
+});
+
+const utf8Decoder = new TextDecoder("utf-8");
+let crlfRe = /\r\n|\n|\r/gm;
+
+app.ports.connectPort.subscribe(async function({id, baudrate}) {
+    console.log('connectPort', id, baudrate);
+    const port = ports[id];
+    await port.open({ baudRate: baudrate });
+
+    let chunk = "";
+
+    console.log('TODO: connected', id);
+    app.ports.onPortConnected.send(id);
+    while (port.readable) {
+        const reader = port.readable.getReader();
+        let startIndex = 0;
+        try {
+            while (true) {
+                const { value, done } = await reader.read();
+                if (done) {
+                    // |reader| has been canceled.
+                    break;
+                }
+                chunk = chunk + utf8Decoder.decode(value, { stream: true });
+                // console.log(`chunk [${chunk}]`, [chunk]);
+                for(;;) {
+                    if(!chunk || chunk === '') break;
+                    let result = crlfRe.exec(chunk);
+                    // console.log(`result [${result}]`, [result]);
+                    if(!result) {
+                        //console.log(`result [${result}]`, [result]);
+                        break;
+                        //continue;
+                    }
+                    let line = chunk.substring(startIndex, result.index);
+                    chunk = chunk.substring(result.index + result[0].length);
+                    // let line = chunk.substr(0);
+                    console.log(`line [${line}]`, [line]);
+                    // const timestamp = +new Date();
+                    app.ports.onPortReceive.send({id/*, timestamp*/, data: line});
+                    break;
+                }
+
+                // Do something with |value|…
+                // console.log(`UART${id}: [${value}]`);
+            }
+        } catch (error) {
+            // Handle |error|…
+            console.log(`UART${id}: error [${error}]`);
+        } finally {
+            reader.releaseLock();
+        }
+    }
+    console.log('TODO:disconnected', id);
+    app.ports.onPortDisconnected.send(id);
+});
+
+
+console.log("app------------", app);
+
+      // var win = gui.Window.get();
+      // win.showDevTools();
+      // showDevTools();
+});
+
+
---
+++ scripts/background.js
@@ -0,0 +1,25 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function (details) {
+    console.log('previousVersion', details.previousVersion);
+});
+
+chrome.app.runtime.onLaunched.addListener(function() {
+    chrome.app.window.create('index.html', {
+        // id: 'TTY_Logger_Chrome_WindowID',
+        // frame: 'none',
+        bounds: {
+            width: 980,
+            height: 600
+        },
+        // frame: 'none',
+        minWidth: 512,
+        minHeight: 256
+    });
+});
+
+chrome.app.window.onClosed.addListener(function() {
+    console.log('EXIT===========================');
+});
+
+console.log('\'Allo \'Allo! Event Page');
